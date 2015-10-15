fe56f4e730ee71903494335e0892fc4c14086b64
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-10-15 10:22:31 -0700

    Add discretizing

--- bonvoyage/bonvoyage.py
+++ bonvoyage/bonvoyage.py
@@ -1,23 +1,116 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 import pandas as pd
 from sklearn.decomposition import NMF
+
+def bin_range_strings(bins):
+    """Given a list of bins, make a list of strings of those bin ranges
+
+    Parameters
+    ----------
+    bins : list_like
+        List of anything, usually values of bin edges
+
+    Returns
+    -------
+    bin_ranges : list
+        List of bin ranges
+
+    >>> bin_range_strings((0, 0.5, 1))
+    ['0-0.5', '0.5-1']
+    """
+    return ['{}-{}'.format(i, j) for i, j in zip(bins, bins[1:])]
+
+
+def binify(data, bins):
+    """Makes a histogram of each column the provided binsize
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        A samples x features dataframe. Each feature (column) will be binned
+        into the provided bins
+    bins : iterable
+        Bins you would like to use for this data. Must include the final bin
+        value, e.g. (0, 0.5, 1) for the two bins (0, 0.5) and (0.5, 1).
+        nbins = len(bins) - 1
+
+    Returns
+    -------
+    binned : pandas.DataFrame
+        An nbins x features DataFrame of each column binned across rows
+    """
+    if bins is None:
+        raise ValueError('"bins" cannot be None')
+    binned = df.apply(lambda x: pd.Series(np.histogram(x, bins=bins)[0]))
+    binned.index = bin_range_strings(bins)
+
+    # Normalize so each column sums to 1
+    binned = binned / binned.sum().astype(float)
+    return binned
+
 
 class VoyageSpace(object):
 
     n_components = 2
-    _binsize = 0.1
+    binsize = 0.1
+    bins = np.arange(0, 1+binsize, binsize)
 
-    included_label = '~1'
-    excluded_label = '~0'
+    near1_label = '~1'
+    near0_label = '~0'
+
+    seed_data = None
+
+    def __init__(self):
+        self.nmf = NMF(n_components=self.n_components, init='nndsvdar')
+        self.nmf.fit(self.seed_data)
+
+    def transform(self, data):
+        """Project the fraction-based data into voyage space using NMF
+
+        Use non-negative matrix factorization (NMF) to transform fraction-based
+        data (ranging from 0 to 1) into a two-dimensional space for
+        visualization and interpretation.
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            A (samples, features) array of data which are composed of
+            fraction-based units (or scaled-down percent based units) which
+            range from 0 to 1
+
+        Returns
+        -------
+        transformed : pandas.DataFrame
+            A (features, 2) array of the data transformed by NMF, and scaled
+            such that the maximum x- and y-values are 1.
+
+        Raises
+        ------
+        ValueError
+            If the data contains any values that are greater than 1 or less
+            than 0.
+        """
+        if (data > 1).any().any():
+            raise ValueError("Some of the data is greater than 1 - only values"
+                             " between 0 and 1 are accepted")
+        if (data < 0).any().any():
+            raise ValueError("Some of the data is less than 0 - only values"
+                             " between 0 and 1 are accepted")
+
+        binned = self.binify(data).T
+        transformed = self.nmf.transform(binned)
+        transformed = pd.DataFrame(transformed, index=data.columns)
+
+        # Normalize data so maximum for x and y axis is always 1. Since
+        # transformed data is non-negative, don't need to subtract the minimum,
+        # since the minimum >= 0.
+        transformed = transformed/transformed.max()
+
+        return transformed
 
     def binify(self, data):
-        return super(SplicingData, self).binify(data, self.bins)
-
-    def nmf(self, data, n_components=2, **kwargs):
-        reducer = NMF(n_components=n_components, **kwargs)
-        reduced = pd.DataFrame(reducer.fit_transform(self.binify(data).T),
-                               index=data.columns)
-        return reduced
+        return binify(data, self.bins)
 
     def binned_nmf_reduced(self, sample_ids=None, feature_ids=None,
                            data=None):
@@ -43,13 +136,13 @@ class VoyageSpace(object):
     
     def _nmf_space_xlabel(self, groupby):
         if self._is_nmf_space_x_axis_excluded(groupby):
-            return self.excluded_label
+            return self.near0_label
         else:
-            return self.included_label
+            return self.near1_label
     
     
     def _nmf_space_ylabel(self, groupby):
         if self._is_nmf_space_x_axis_excluded(groupby):
-            return self.included_label
+            return self.near1_label
         else:
-            return self.excluded_label
+            return self.near0_label
