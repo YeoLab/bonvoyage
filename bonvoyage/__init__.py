b46daf267d508d94c58ddc381bf9d1f360dad495
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-11-16 15:14:47 -0800

    remove binify from accessible functions

--- bonvoyage/__init__.py
+++ bonvoyage/__init__.py
@@ -4,11 +4,8 @@ __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-
-from .infotheory import binify
 from .voyages import Voyages
 from .visualize import waypointplot
 from .waypoints import Waypoints
 
-__all__ = ['Waypoints', 'voyages', 'direction', 'waypointplot',
-           'binify']
+__all__ = ['Waypoints', 'voyages', 'direction', 'waypointplot']
